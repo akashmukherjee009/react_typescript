9c79f8723daa61edecfd95f9e0df7e582eec8add
akashmukherjee009 <mukherjeeakash009@gmail.com>
2025-12-21 00:57:27 +0530

    Types of TS

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,19 @@
 function App() {
+  let name: string;
+  let age: number;
+  let isStudent: boolean;
+  let hobbies: string[];
+  let role: [string, number]; //tuple
+  // object
+  let person: Object;
+  type car = {
+    name: string;
+    isVoter?: number; //use ? for optional
+  };
 
+  // Array of objects
+  let parking: car[];
+  name = "Akash";
   return <>Hello World</>;
 }
 
