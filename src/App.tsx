7a56f3066eb254e809ea1f9bf198ffdd6be576bc
akashmukherjee009 <mukherjeeakash009@gmail.com>
2025-12-21 01:05:20 +0530

    function in TS

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,25 @@
 function App() {
-  let name: string;
-  let age: number | string; // Union Type
-  let isStudent: boolean;
-  let hobbies: string[];
-  let role: [string, number]; //tuple
-  // object
-  let person: Object;
-  type car = {
-    name: string;
-    isVoter?: number; //use ? for optional
-  };
+  // let name: string;
+  // let age: number | string; // Union Type
+  // let isStudent: boolean;
+  // let hobbies: string[];
+  // let role: [string, number]; //tuple
+  // // object
+  // let person: Object;
+  // type car = {
+  //   name: string;
+  //   isVoter?: number; //use ? for optional
+  // };
+  let name: any;
+  let person: unknown;
+  // // Array of objects
+  // let parking: car[];
+  // name = "Akash";
 
-  // Array of objects
-  let parking: car[];
-  name = "Akash";
+  //Type of function
+  let func1: Function;
+  let func2: (name: string) => void; //Perf
+  let func3: (name: string) => never;
   return <>Hello World</>;
 }
 
